70f607983900a33f7ef4d7417e0d414ea83af536
souvik <souvik.jgec13@gmail.com>
2021-06-15 14:57:23 +0530

    first ds chnagess

---
+++ datastructure/src/main/java/com/souvik/ds/ArrayReverse.java
@@ -0,0 +1,26 @@
+package com.souvik.ds;
+
+import java.util.Arrays;
+
+public class ArrayReverse {
+
+	public static void main(String[] args) {
+		 
+		int[] arr = new int[] {1,2,4,6,8};
+		int low =0;
+		int high = arr.length-1;
+
+		
+		while (low < high) {
+			int tmp = arr[low];
+			arr[low] = arr[high];
+			arr[high] = tmp;
+			low++;
+			high--;
+		}
+		
+		Arrays.stream(arr).forEach(data -> System.out.println(data));
+
+	}
+
+}
